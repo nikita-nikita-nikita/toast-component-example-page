9fe15dc8036194edf6bb59a4c24ab235d61869a3
Nikita <neymarenko123@gmail.com>
2020-11-15 20:28:07 +0200

    create simple example page

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import "nikita-toast-component/src/styles/index.css";
+import "./styles/main.css";
+import {generateManagerWithContent} from "nikita-toast-component";
+
+const manger = generateManagerWithContent("Hello, that's example page, enjoy", {
+    title: 'im title',
+    status: 'success',
+});
+
+let title = 'im title';
+
+let duration = 0
+
+document.querySelector('.message')
+    .addEventListener('submit', e => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    manger.createNew(data.get('message'), title)
+});
+
+document.querySelector('#duration-input')
+    .addEventListener('input', ({target}) => {
+    duration = +target.value;
+});
+
+document.querySelector('#set-duration')
+    .addEventListener('click', () => {
+    manger.options.autoCloseDuration = duration;
+});
+
+document.querySelector('#status-select')
+    .addEventListener('change', ({target}) => {
+    manger.options.status = target.value
+} )
